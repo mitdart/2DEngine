b5707bafa72ae715a3de0ae73b78fa4b5090c1ae
Dyomkin Dmitry <34719599+DartShweider@users.noreply.github.com>
2019-02-13 22:47:47 +0300

    Add files via upload

--- hackertyper.cpp
+++ hackertyper.cpp
@@ -9,17 +9,79 @@ HackerTyper::HackerTyper(int ScrResX, int ScrResY, std::string title)
     Title = title;
 }
 
+void DrawTmpText(sf::RenderWindow* window, std::string fontPath, std::string TmpText)
+{
+    sf::Font font;
+    font.loadFromFile(fontPath);
+
+    sf::Text text(TmpText, font, 15);
+
+    text.setOutlineColor(sf::Color::Red);
+    text.setFillColor(sf::Color::Red);
+
+    text.setPosition(0, 0);
+
+
+
+    window->draw(text);
+
+}
+
+
+void Scroll(sf::RenderWindow* window, sf::View* view, int Edge, float Velocity)
+{
+
+    sf::Vector2i localPosition = sf::Mouse::getPosition(*window);
+
+    /*
+    if (localPosition.x < edge) {view.move(-vel, 0); }
+    if (localPosition.x > window.getSize().x - edge) {view.move(vel, 0); }
+    */
+
+    if (localPosition.y > window->getSize().y - Edge) {view->move(0, Velocity); }
+    if (localPosition.y < Edge) {view->move(0, -Velocity); }
+
+}
+
+void TransformText(int* counter, int* indicator, std::string* AppText, std::string* TmpText)
+{
+
+    if (*indicator == 0)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if ((*AppText)[j + *(counter)*4] == *"\0")
+            {
+                *indicator = 1;
+                break;
+            }
+            else
+            {
+                *TmpText += (*AppText)[j + (*counter)*4];
+            }
+        }
+        if(*indicator == 0){(*counter)++;}
+
+    }
+
+    fflush(0);
+
+
+}
+
+
+
+
 
 void HackerTyper::AppRun()
 {
     sf::RenderWindow window(sf::VideoMode(Resolution.x, Resolution.y), Title);
-
     sf::View view( sf::FloatRect(0,0,1600,900));
 
-    sf::Font font;
-    font.loadFromFile("arial.ttf");
 
-    int i = 0;
+
+
+    int counter = 0;
     int indicator = 0;
 
     while(window.isOpen())
@@ -32,52 +94,22 @@ void HackerTyper::AppRun()
 
             if (event.type == sf::Event::KeyPressed)
             {
-
-                if (indicator == 0)
-                {
-                    for (int j = 0; j < 4; j++)
-                    {
-                        if (AppText[j + i*4] == *"\0")
-                        {
-                            indicator = 1;
-                            break;
-                        }
-                        else
-                        {
-                            TmpText += AppText[j + i*4];
-                        }
-                    }
-                    i++;
-                }
-
-                fflush(0);
+                TransformText(&counter, &indicator, &AppText, &TmpText);
             }
         }
 
-        sf::Text text(TmpText, font, 15);
-        text.setOutlineColor(sf::Color::Red);
-        text.setFillColor(sf::Color::Red);
-
-
-        sf::Vector2i localPosition = sf::Mouse::getPosition(window);
-
-        /*
-        if (localPosition.x < edge) {view.move(-vel, 0); }
-        if (localPosition.x > window.getSize().x - edge) {view.move(vel, 0); }
-        */
-
-        if (localPosition.y > window.getSize().y - Edge) {view.move(0, Velocity); }
-        if (localPosition.y < Edge) {view.move(0, -Velocity); }
-
-        text.setPosition(0, 0);
+        float Velocity = 0.05;
+        int Edge = 30;
 
         window.clear();
+        DrawTmpText(&window, "arial.ttf", TmpText);
+        Scroll(&window, &view, 30, 0.05);
 
-        window.draw(text);
+
+        window.setView(view);
 
         window.display();
 
-        window.setView(view);
 
     }
 
