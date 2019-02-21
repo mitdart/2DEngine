954b9a66ff882831a5940a0775c7dc56fda1ada4
DartShweider <armada.tankov@yandex.ru>
2019-02-21 22:57:06 +0300

    testing

--- drawmanager.cpp
+++ drawmanager.cpp
@@ -18,12 +18,9 @@ void DrawManager::DrawTmpText()
 
 
     sf::Text text(dataStorage->TmpText, dataStorage->font, 15);
-
     text.setOutlineColor(sf::Color::Red);
     text.setFillColor(sf::Color::Red);
-
     text.setPosition(0, 0);
-
     window->draw(text);
 }
 
