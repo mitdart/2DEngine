a2e7f4d1590f7e1c66701a2c52c5045911832a8f
DartShweider <armada.tankov@yandex.ru>
2019-02-22 00:26:14 +0300

    created graphObject

---
+++ gameobject.h
@@ -0,0 +1,19 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+#include <string>
+
+class GameObject
+{
+public:
+
+    GameObject();
+    std::string name;
+
+
+    std::vector<GameObjectComponent*> OBJComponents;
+
+
+
+};
+
+#endif // GAMEOBJECT_H
