61261f12bf8627fbdd32842c04c6a3a7ecf0b9d1
DartShweider <armada.tankov@yandex.ru>
2019-03-11 15:45:48 +0300

    added transorm_text function for testing. It works!

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -32,10 +32,9 @@ void Application::applicationInit()
     //TextInit
     this->dataStorage->gameObjects.insert({"textik", new TextObject("arial.ttf")});
     this->dataStorage->gameObjects.find("textik")->second->renderer = new Renderer(new sf::Text(static_cast<TextObject&>(*this->dataStorage->gameObjects.find("textik")->second).text,
-                                                                                               static_cast<TextObject&>(*this->dataStorage->gameObjects.find("textik")->second).font,
-                                                                                               static_cast<TextObject&>(*this->dataStorage->gameObjects.find("textik")->second).size));
+                                                                                                static_cast<TextObject&>(*this->dataStorage->gameObjects.find("textik")->second).font,
+                                                                                                static_cast<TextObject&>(*this->dataStorage->gameObjects.find("textik")->second).size));
     static_cast<sf::Text&>(*this->dataStorage->gameObjects.find("textik")->second->renderer->mesh).setPosition(0,0);
-
 
     //LogoInit
     this->dataStorage->gameObjects.insert({"picture", new Picture("logo.png")});
--- src/core/handlers/eventhandler.h
+++ src/core/handlers/eventhandler.h
@@ -1,23 +1,14 @@
 #ifndef EVENTHANDLER_H
 #define EVENTHANDLER_H
 #include <iostream>
-
-
 #include <SFML/Graphics.hpp>
-
-struct TransformOptions
-{
-  int counter = 0;
-  int indicator = 0;
-};
 
 class EventHandler
 {
 public:
     EventHandler();
-
     void EHClose();
-    TransformOptions options;
+
     void TransformText();
     void pass(){}
 
