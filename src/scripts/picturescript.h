ac090711210de90bf831a21542ffdb5245781548
Gleb Shuldyakov <33556795+sirion34@users.noreply.github.com>
2019-04-04 14:56:03 +0300

    Add files via upload

--- src/scripts/picturescript.h
+++ src/scripts/picturescript.h
@@ -1,13 +1,16 @@
 #ifndef PICTURESCRIPT_H
 #define PICTURESCRIPT_H
 #include "../core/objects/basicscript.h"
+#include "../core/storage/datastorage.h"
 #include "SFML/Graphics.hpp"
 
 class PictureScript : public BasicScript
 {
 public:
+    PictureScript(std::string imagePath);
     void update();
-
+    sf::Texture pictureTexture;
+    sf::Sprite pictureSprite;
 };
 
 #endif // PICTURESCRIPT_H
